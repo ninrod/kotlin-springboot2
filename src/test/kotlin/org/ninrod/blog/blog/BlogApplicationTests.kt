0b5f0a2c88c052e9d3c772409590c0c603542487
ninrod <filipe.silva@gmail.com>
2018-08-05 00:17:30 -0300

    implement junit 5 integration

--- src/test/kotlin/org/ninrod/blog/blog/BlogApplicationTests.kt
+++ src/test/kotlin/org/ninrod/blog/blog/BlogApplicationTests.kt
@@ -1,16 +1,15 @@
 package org.ninrod.blog.blog
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
+// pq isso não funciona?
+//@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class BlogApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+  @Test
+  fun contextLoads() {
+  }
 }
