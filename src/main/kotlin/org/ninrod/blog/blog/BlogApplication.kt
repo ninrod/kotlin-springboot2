69cfbc95e340a818cab4e4340a93a05f00b2ad07
ninrod <filipe.silva@gmail.com>
2018-08-07 23:01:08 -0300

    add first rest endpoint

--- src/main/kotlin/org/ninrod/blog/blog/BlogApplication.kt
+++ src/main/kotlin/org/ninrod/blog/blog/BlogApplication.kt
@@ -2,10 +2,22 @@ package org.ninrod.blog.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
-@SpringBootApplication
-class BlogApplication
-
+@SpringBootApplication class BlogApplication
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args)
+}
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingController {
+    val counter = AtomicLong()
+    @GetMapping("/")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
 }
