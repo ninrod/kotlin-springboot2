eb526d69bee265bf3ab134036a70a259cd5db2db
ninrod <filipe.silva@gmail.com>
2018-08-15 23:06:30 -0300

    fix logging

--- src/main/kotlin/org/ninrod/blog/BlogApplication.kt
+++ src/main/kotlin/org/ninrod/blog/BlogApplication.kt
@@ -1,9 +1,23 @@
 package org.ninrod.blog
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-@SpringBootApplication class BlogApplication
+@SpringBootApplication
+class BlogApplication : SpringBootServletInitializer() {
+    companion object {
+        val logger = LoggerFactory.getLogger(BlogApplication.javaClass)
+    }
+    override fun createSpringApplicationBuilder(): SpringApplicationBuilder {
+        System.out.println("INIT")
+        logger.info("Initializing")
+        return SpringApplicationBuilder()
+    }
+}
+
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args)
 }
