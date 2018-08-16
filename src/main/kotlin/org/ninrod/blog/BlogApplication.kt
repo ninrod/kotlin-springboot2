d1cdfab239b0d7d302e30d75bd1bdf2b477c069b
ninrod <filipe.silva@gmail.com>
2018-08-16 10:26:15 -0300

    further improve logging capabilities

--- src/main/kotlin/org/ninrod/blog/BlogApplication.kt
+++ src/main/kotlin/org/ninrod/blog/BlogApplication.kt
@@ -1,20 +1,20 @@
 package org.ninrod.blog
 
 import org.slf4j.LoggerFactory
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class BlogApplication : SpringBootServletInitializer() {
     companion object {
         val logger = LoggerFactory.getLogger(BlogApplication.javaClass)
     }
-    override fun createSpringApplicationBuilder(): SpringApplicationBuilder {
-        System.out.println("INIT")
-        logger.info("Initializing")
-        return SpringApplicationBuilder()
+    @PostConstruct
+    fun postConstruct() {
+        BlogApplication.logger.info("-------------------------Blog Initializing---------------------")
     }
 }
 
