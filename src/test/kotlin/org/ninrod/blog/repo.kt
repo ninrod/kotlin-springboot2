0b27592083bcb087562bde2c47f5e7c4afdc4ce6
ninrod <filipe.silva@gmail.com>
2018-08-16 21:50:28 -0300

    closes #6: drop dependency on h2

--- src/test/kotlin/org/ninrod/blog/repo.kt
+++ src/test/kotlin/org/ninrod/blog/repo.kt
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 class RepositoriesTests(@Autowired val entityManager: TestEntityManager,
                         @Autowired val userRepository: UserRepository) {
 
