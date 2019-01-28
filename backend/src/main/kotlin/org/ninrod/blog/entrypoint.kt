1e157f765fd713f1f8f4b7ce851301a9cd65fecd
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-01-28 10:03:32 -0200

    add flexibility to bootstrap logic

--- backend/src/main/kotlin/org/ninrod/blog/entrypoint.kt
+++ backend/src/main/kotlin/org/ninrod/blog/entrypoint.kt
@@ -1,14 +1,19 @@
 package org.ninrod.blog
 
-import org.springframework.boot.runApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import javax.annotation.PostConstruct
 
 
 fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args)
+    SpringApplicationBuilder()
+            .bannerMode(Banner.Mode.CONSOLE)
+            .sources(BlogApplication::class.java)
+            .run(*args)
+
 }
 
 @SpringBootApplication
