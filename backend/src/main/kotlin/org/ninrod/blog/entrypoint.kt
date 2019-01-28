e7abc5389c2af8db4ef4fad6b3ea5ca0fab0ce31
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-01-28 09:20:03 -0200

    move app to entrypoint

--- backend/src/main/kotlin/org/ninrod/blog/entrypoint.kt
+++ backend/src/main/kotlin/org/ninrod/blog/entrypoint.kt
@@ -1,7 +1,23 @@
 package org.ninrod.blog
 
 import org.springframework.boot.runApplication
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import javax.annotation.PostConstruct
+
 
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args)
+}
+
+@SpringBootApplication
+class BlogApplication {
+    companion object {
+        val logger: Logger? = LoggerFactory.getLogger(BlogApplication::class.java)
+    }
+    @PostConstruct
+    fun postConstruct() {
+        logger?.info("-------------------------Blog Initializing---------------------")
+    }
 }
